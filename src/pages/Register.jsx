30bbcff779d3629fb636dba7f460c8c42c00aed6
hasanali207 <hasanali142228@gmail.com>
2024-04-08 16:13:32 +0600

    added register pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react';
+import Navbar from '../components/Navbar';
+import { NavLink } from 'react-router-dom';
 
-const Login = () => {
-    const handleLogin= (e) =>{
+const Register = () => {
+    const handleRegister= (e) =>{
         e.preventDefault(); 
         const form = new FormData(e.currentTarget)
         const email = form.get('email')
         const password = form.get('password')
 
         console.log(email, password);
-    } 
+        
 
+    }
 
-  return (
-    <>
+    return (
+        <>
       <Navbar></Navbar>
 
     
         <div className="hero-content bg-blue rounded-2xl min-h-[calc(100vh-80px)]">
           <div className="card shadow-2xl bg-base-100 w-full md:w-1/2 lg:w-1/3">
-            <h1 className="text-center text-2xl text-black font-semibold mt-6">Please Login</h1>
-            <form onSubmit={handleLogin} className="card-body p-6">
+            <h1 className="text-center text-2xl text-black font-semibold mt-6">Please Register</h1>
+            <form onSubmit={handleRegister} className="card-body p-6">
               <div className="form-control ">    
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -56,14 +57,13 @@ const Login = () => {
 
                 </div>
 
-                <p>Create An Account? <NavLink  className="text-black" to='/register'>Register</NavLink></p>
+                <p>Already Have An Account? <NavLink  className="text-black" to='/login'>Login</NavLink></p>
             </form>
             
           </div>
         </div>
       </>
-   
-  );
+    );
 };
 
-export default Login;
+export default Register;
--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -3,6 +3,7 @@ import Root from "../layout/Root";
 import Home from "../pages/Home";
 import ErrorPage from "../components/ErrorPage";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
         {
             path: "/login",
             element: <Login></Login>,
+        },
+        {
+            path: "/register",
+            element: <Register></Register>,
         }
       ]
     },
